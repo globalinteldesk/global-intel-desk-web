9bd367c18cd491d7d4c0b7cfaf59f2a836460df9
Akshay <akshay@trustedcommunities.org>
2025-07-13 12:30:08 +0530

    Add satellite picture and news section
    Add a satellite picture with animation and a new section to display the latest news.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,6 @@ import { Navigation } from '@/components/Navigation';
 import { HeroSection } from '@/components/HeroSection';
 import { IntelligenceSection } from '@/components/IntelligenceSection';
 import { GlobalAnalysisSection } from '@/components/GlobalAnalysisSection';
-import { NewsSection } from '@/components/NewsSection';
 import { Footer } from '@/components/Footer';
 
 const Index = () => {
@@ -13,7 +12,6 @@ const Index = () => {
         <HeroSection />
         <IntelligenceSection />
         <GlobalAnalysisSection />
-        <NewsSection />
       </main>
       <Footer />
     </div>
